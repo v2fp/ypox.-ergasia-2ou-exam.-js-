6bae4368dcfdbeca72fdbeb20a5e42d3f089091c
Koutris06 <n.koutris06@gmail.com>
2025-05-03 18:58:43 +0300

    class loan

--- JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/Loan.java
+++ JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/Loan.java
@@ -1,9 +1,86 @@
 package lab.unipi.gui.JavaFXLab;
+import java.time.LocalDate; 
+import java.time.temporal.ChronoUnit;
 
 public class Loan {
-
-	public Loan() {
-		// TODO Auto-generated constructor stub
+	private String id;
+	private User user;
+	private Book book; 
+	private LocalDate loanDate;
+	private LocalDate dueDate;
+	private LocalDate returnDate;
+	private Fine fine; 
+	
+	public Loan(String id, User user, Book book, LocalDate loanDate, LocalDate dueDate) {
+			this.id = id;
+			this.user = user;
+			this.book = book;
+			this.loanDate = loanDate;
+			this.dueDate = dueDate; 
+			this.returnDate = null; 
+			this.fine = null; 
+		
 	}
+	public boolean Overdue() {
+			
+	}
+	
+    // setters & getters
+    public String getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public Fine getFine() {
+        return fine;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setFine(Fine fine) {
+        this.fine = fine;
+    }
+	
 
 }
