43f1acb36dd614850f8a2c057a6acfa228bc33c7
v2 <locogreco64@gmail.com>
2025-05-18 12:56:36 +0300

    restored classes

--- JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/App.java
+++ JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/App.java
@@ -7,22 +7,25 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-	Stage primaryStage;
-	Scene mainScene;
+    static Stage primaryStage;
+    static Scene mainScene, bookScene;
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        
+
         SceneCreator mainSceneCreator = new MainSceneCreator(650, 300);
         mainScene = mainSceneCreator.createScene();
-        
+
         SceneCreator BookSceneCreator = new BookSceneCreator(650, 300);
-        BookScene = BookSceneCreator.createScene();
+        bookScene = BookSceneCreator.createScene();
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("LibraryMainFX Window");
+        primaryStage.show();
+
+        SceneCreator StudentSceneCreator = new StudentSceneCreator(650, 300);
+        studentScene = StudentSceneCreator.createScene();
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("LibraryMainFX Window");
         primaryStage.show();
--- JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/MainSceneCreator.java
+++ JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/MainSceneCreator.java
@@ -1,9 +1,7 @@
 package lab.unipi.gui.JavaFXLab;
 
 import java.util.concurrent.Flow;
-
 import javafx.scene.input.MouseEvent;
-
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,51 +9,52 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 
 public class MainSceneCreator extends SceneCreator implements EventHandler<MouseEvent> {
-	// Flow Pane (root mode
-	FlowPane rootFlowPane;
-	// Main scene buttons
-	Button bookBtn, studentBtn, loanBtn, paymentBtn;
-	public MainSceneCreator(double width, double height) {
-		super(width, height);
-		super(width, height);
-		rootFlowPane = new FlowPane();
-		bookBtn = new Button("Book");
-		studentBtn = new Button("Student");
-		loanBtn = new Button("Loan");
-		paymentBtn = new Button("Payment");
-		
-		// attach handle event to bookBtn;
-		bookBtn.setOnMouseClicked(this);
-		
-		//set up Flow pane
-		rootFlowPane.setHgap(10);
-		rootFlowPane.setAlignment(Pos.CENTER);
-		// add book, student, loan and payment buttons to rootFlowPane
-		rootFlowPane.getChildren().add(bookBtn);
-		rootFlowPane.getChildren().add(studentBtn);
-		rootFlowPane.getChildren().add(loanBtn);
-		rootFlowPane.getChildren().add(paymentBtn);
-	}
-	@Override
-	Scene createScene() { return new Scene (rootFlowPane, width, height); }
-	
-	@Override
-	public void handle(lab.unipi.gui.JavaFXLab.MouseEvent event) {
-		if(event.getSource()== bookBtn) {
-			App.primaryStage.setScene(App.bookScene);
-			App.primaryStage.setScene("Book window");
-		}
-		
-	}
-	@Override
-	public void handle(lab.unipi.gui.JavaFXLab.MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void handle(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+    // Flow Pane (root mode
+    FlowPane rootFlowPane;
+    // Main scene buttons
+    Button bookBtn, studentBtn, loanBtn, paymentBtn;
+    public MainSceneCreator(double width, double height) {
+        super(width, height);
+        rootFlowPane = new FlowPane();
+        bookBtn = new Button("Book");
+        studentBtn = new Button("Student");
+        loanBtn = new Button("Loan");
+        paymentBtn = new Button("Payment");
 
+        // attach handle event to bookBtn;
+        bookBtn.setOnMouseClicked(this);
+
+        //set up Flow pane
+        rootFlowPane.setHgap(50);
+        rootFlowPane.setAlignment(Pos.CENTER);
+        // add book, student, loan and payment buttons to rootFlowPane
+        rootFlowPane.getChildren().add(bookBtn);
+        rootFlowPane.getChildren().add(studentBtn);
+        rootFlowPane.getChildren().add(loanBtn);
+        rootFlowPane.getChildren().add(paymentBtn);
+    }
+    @Override
+    Scene createScene() {
+    return new Scene (rootFlowPane, width, height); }
+
+    @Override
+    public void handle(MouseEvent event) {
+        if(event.getSource() == bookBtn) {
+            App.primaryStage.setScene(App.bookScene);
+            App.primaryStage.setTitle("Book window"); 
+        }
+        if(event.getSource() == studentBtn); {
+        	App.primaryStage.setScene(App.studentScene);
+            App.primaryStage.setTitle("Student window"); 
+        }
+        if(event.getSource() == loanBtn); {
+            App.primaryStage.setScene(App.loanScene);
+            App.primaryStage.setTitle("Loan window"); 
+        }
+        if(event.getSource() == paymentBtn); {
+            App.primaryStage.setScene(App.paymentScene);
+            App.primaryStage.setTitle("Payment window"); 
+        }
+    }
 }
+
