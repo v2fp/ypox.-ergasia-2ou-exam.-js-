a760ef3c38548294b4da94860578af187f8628c8
Gerardis22 <gerardis.stav@gmail.com>
2025-05-19 17:08:58 +0300

    test
    mhtso dose to ok

--- JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/Librarian.java
+++ JavaFXLab/src/main/java/lab/unipi/gui/JavaFXLab/Librarian.java
@@ -1,9 +1,40 @@
 package lab.unipi.gui.JavaFXLab;
 
-public class Librarian {
+	public class Librarian extends User {
+	    private String workDepartment;
+	    private String educationLevel;
 
-	public Librarian() {
-		// TODO Auto-generated constructor stub
+	    // Constructor
+	    public Librarian(int id, String firstName, String lastName, String phone, String email, String birthDate,
+	                     String workDepartment, String educationLevel) {
+	        super(id, firstName, lastName, phone, email, birthDate);
+	        this.workDepartment = workDepartment;
+	        this.educationLevel = educationLevel;
+	    }
+
+	    // Getters
+	    public String getWorkDepartment() {
+	        return workDepartment;
+	    }
+
+	    public String getEducationLevel() {
+	        return educationLevel;
+	    }
+
+	    // Setters
+	    public void setWorkDepartment(String workDepartment) {
+	        this.workDepartment = workDepartment;
+	    }
+
+	    public void setEducationLevel(String educationLevel) {
+	        this.educationLevel = educationLevel;
+	    }
+
+	    // toString
+	    @Override
+	    public String toString() {
+	        return super.toString() + "\n" +
+	               "Work Department: " + workDepartment + "\n" +
+	               "Education Level: " + educationLevel;
+	    }
 	}
-
-}
